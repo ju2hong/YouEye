988774bea62a6dcf8b731eb143360428ba25cd28
ju2hong <152269299+ju2hong@users.noreply.github.com>
2024-05-29 18:46:07 +0900

    팝업창 제작

--- app/src/main/java/com/example/youeye/IDLoginActivity.java
+++ app/src/main/java/com/example/youeye/IDLoginActivity.java
@@ -2,23 +2,33 @@ package com.example.youeye;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class IDLoginActivity extends AppCompatActivity {
 
     private List<ImageButton> imageButtons;
+    private StringBuilder inputNumber; // 사용자가 입력한 숫자를 저장할 StringBuilder
     private int currentIndex = 0;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_idlogin);
+
+        inputNumber = new StringBuilder(); // StringBuilder 초기화
 
         // ImageButton을 리스트로 초기화합니다.
         imageButtons = new ArrayList<>();
@@ -94,27 +104,53 @@ public class IDLoginActivity extends AppCompatActivity {
                 enteredNumber.append(number);
             }
         }
+        // LayoutInflater를 사용하여 custom_dialog_layout.xml 파일을 inflate합니다.
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View dialogView = inflater.inflate(R.layout.custom_dialog_layout, null);
 
+        // AlertDialog.Builder를 사용하여 다이얼로그를 생성합니다.
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("아이디 확인");
-        builder.setMessage("입력된 숫자가 맞습니까? " + enteredNumber.toString());
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+        builder.setView(dialogView);
+
+
+        // 다이얼로그 내의 TextView에 제목과 메시지를 설정합니다.
+        TextView titleTextView = dialogView.findViewById(R.id.dialog_title);
+        TextView messageTextView = dialogView.findViewById(R.id.dialog_message);
+        titleTextView.setText("아이디 확인");
+        messageTextView.setText("\"" + enteredNumber.toString() + "\"\n맞습니까? ");
+
+        // 예 버튼에 대한 클릭 리스너를 설정합니다.
+        ImageButton yesButton = dialogView.findViewById(R.id.yesButton);
+        yesButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
+            public void onClick(View v) {
                 // 사용자가 Yes를 선택한 경우, LoginActivity로 화면을 전환합니다.
                 Intent intent = new Intent(IDLoginActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+// 아니오 버튼에 대한 클릭 리스너를 설정합니다.
+        ImageButton noButton = dialogView.findViewById(R.id.noButton);
+        noButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // 사용자가 No를 선택한 경우, 입력된 숫자를 다시 수정할 수 있도록 처리할 수 있습니다.
+            public void onClick(View v) {
+                // 사용자가 No를 선택한 경우, 입력된 숫자를 다시 초기화합니다.
                 clearAllImageButtons(); // 모든 이미지 버튼을 초기화합니다.
+                inputNumber.setLength(0); // 입력된 숫자를 초기화합니다.
+                dialog.dismiss(); // 팝업창을 닫습니다
             }
         });
-        builder.show();
+
+
+        // AlertDialog를 생성하고 표시합니다.
+        dialog = builder.create();
+        // 배경 설정
+        Drawable background = getResources().getDrawable(R.drawable.custom_dialog_background);
+        dialog.getWindow().setBackgroundDrawable(background);
+        dialog.show();
     }
+
 
     // 모든 ImageButton을 초기화하는 메서드
     public void clearAllImageButtons() {
@@ -126,4 +162,3 @@ public class IDLoginActivity extends AppCompatActivity {
     }
 
 }
-
