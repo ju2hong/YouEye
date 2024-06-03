3d4afc2d376be53b114b503e6d75f2d1ac828336
ju2hong <152269299+ju2hong@users.noreply.github.com>
2024-06-03 18:14:03 +0900

    페이지간 버튼 연결

--- app/src/main/java/com/example/youeye/MyPageActivity.java
+++ app/src/main/java/com/example/youeye/MyPageActivity.java
@@ -1,7 +1,9 @@
 package com.example.youeye;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyPageActivity extends AppCompatActivity {
@@ -9,5 +11,15 @@ public class MyPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        // 알람 버튼 참조
+        ImageButton alButton = findViewById(R.id.alBtton);
+        alButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyPageActivity.this, TimePickerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
