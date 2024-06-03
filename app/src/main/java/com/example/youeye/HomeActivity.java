bf006eeccb39925da74830b21c00417d5b082501
ju2hong <152269299+ju2hong@users.noreply.github.com>
2024-06-03 18:01:50 +0900

    마이페이지 연결

--- app/src/main/java/com/example/youeye/HomeActivity.java
+++ app/src/main/java/com/example/youeye/HomeActivity.java
@@ -2,9 +2,10 @@ package com.example.youeye;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.TextView;
+
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
     @Override
@@ -12,6 +13,14 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
+        // 마이페이지 버튼 참조
+        ImageButton MyPage = findViewById(R.id.MyPage);
+        MyPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MyPageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
