64ee190ab0df3c944f99daffedaab93712fedb5a
ju2hong <152269299+ju2hong@users.noreply.github.com>
2024-09-21 16:11:05 +0900

    사진검색 페이지 뒤로가기버튼 tts

--- app/src/main/java/com/example/youeye/home/ImageSearchActivity.java
+++ app/src/main/java/com/example/youeye/home/ImageSearchActivity.java
@@ -4,19 +4,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.youeye.R;
+import com.example.youeye.TTSManager;
 
 public class ImageSearchActivity extends AppCompatActivity {
+    private TTSManager ttsManager;
+    private ImageButton imageButton4;
+    private TextView textView9;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagesearch);
-    }
-    public void onBackButtonPressed(View view) {
 
+        // TTSManager 초기화
+        ttsManager = new TTSManager(this);
+
+        // View 초기화
+        textView9 = findViewById(R.id.textView9);
+        imageButton4 = findViewById(R.id.imageButton4);  // imageButton4 초기화
+
+        // 뒤로가기 버튼 클릭 이벤트 설정
+        imageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ttsManager.speakTextViewText(textView9);  // textView9의 텍스트를 읽음
+                onBackButtonPressed(v);
+            }
+        });
+    }
+
+    public void onBackButtonPressed(View view) {
         finish(); // 종료하고 이전 액티비티로 돌아감
     }
 }
