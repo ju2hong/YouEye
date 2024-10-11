55acb477c55d90fa315763af7694561320aa40f4
parkgl0512 <parkgl0512@naver.com>
2024-10-11 17:35:43 +0900

    알람소리 파일 커밋

--- app/src/main/java/com/example/youeye/alarm/PlayingService.java
+++ app/src/main/java/com/example/youeye/alarm/PlayingService.java
@@ -7,7 +7,6 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Handler;
