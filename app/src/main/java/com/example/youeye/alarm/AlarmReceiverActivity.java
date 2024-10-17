70be432ec182b8cc482221effbbb27402975e667
parkgl0512 <parkgl0512@naver.com>
2024-10-17 17:51:40 +0900

    알람오류수정

--- app/src/main/java/com/example/youeye/alarm/AlarmReceiverActivity.java
+++ app/src/main/java/com/example/youeye/alarm/AlarmReceiverActivity.java
@@ -3,8 +3,12 @@ package com.example.youeye.alarm;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.example.youeye.R;
 
 public class AlarmReceiverActivity extends BroadcastReceiver {
     @Override
@@ -12,5 +16,21 @@ public class AlarmReceiverActivity extends BroadcastReceiver {
         // 토스트 메시지 표시
         Log.d("AlarmReceiver", "알람이 울렸습니다!");
         Toast.makeText(context, "알람 시간입니다!", Toast.LENGTH_LONG).show();
+
+        // PlayingService 실행
+        Intent serviceIntent = new Intent(context, PlayingService.class);
+        serviceIntent.putExtra("action","START_ALARM");
+
+        if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.O)
+
+        {
+            // Android 8.0 이상에서 Foreground Service 사용
+            context.startForegroundService(serviceIntent);
+        } else
+
+        {
+            context.startService(serviceIntent);
+        }
     }
 }
+
