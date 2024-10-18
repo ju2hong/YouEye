672a4b58831032b64c22bcfea10bf8aaedf37a2d
ju2hong <152269299+ju2hong@users.noreply.github.com>
2024-10-18 14:50:28 +0900

    나의 복용약 activity

---
+++ app/src/main/java/com/example/youeye/home/Show_pill.java
@@ -0,0 +1,16 @@
+package com.example.youeye.home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.youeye.R;
+
+public class Show_pill  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_pill);
+    }
+
+}
